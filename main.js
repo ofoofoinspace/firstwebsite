2b5e1d269f4f9ea5057e42e9a763abcabc0030f2
ofoofoinspace <143392871+ofoofoinspace@users.noreply.github.com>
2023-10-25 22:23:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+let target=document.querySelector("#dynamic");
+let stringArr=["OFOOFO","양 양","건축사사무소","ARCHITECTS"];
+let selectString=stringArr[Math.floor(Math.random()*stringArr.length)];
+let selectStringArr=selectString.split("");
+
+function randomString(){
+    let target=document.querySelector("#dynamic");
+    let stringArr=["OFOOFO","양 양","건축사사무소","ARCHITECTS"];
+    let selectString=stringArr[Math.floor(Math.random()*stringArr.length)];
+    let selectStringArr=selectString.split("");
+    return selectStringArr;
+
+}
+
+//타이핑 리셋
+function resetTyping(){
+    target.textContent="";
+    dynamic(randomString());
+}
+
+
+//한글자씩 텍스트 출력 함수
+function dynamic(randomArr){
+    if(randomArr.length>0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        },180);
+    }else{
+        setTimeout(resetTyping, 3000);
+    }
+}
+
+dynamic(randomString());
+
+console.log(selectString);
+console.log(selectStringArr);
+
+
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink,500);
+
+
